5c5eb965aec294cbc2068ec51f81d88ef08b57bf
Hari <mhariprasanth769@gmail.com>
2019-09-16 17:46:00 +0530

    pwa added

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const cacheName = 'v1::static';
+
+self.addEventListener('install', e => {
+  
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/',
+       
+      ]).then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    // ensure we check the *right* cache to match against
+    caches.open(cacheName).then(cache => {
+      return cache.match(event.request).then(res => {
+        return res || fetch(event.request)
+      });
+    })
+  );
+});
