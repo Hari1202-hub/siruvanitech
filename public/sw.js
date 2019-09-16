ed1970fb3fb73c7a0035145be56ee93dc12b71e5
Hari <mhariprasanth769@gmail.com>
2019-09-16 17:57:01 +0530

    pwa modified

--- public/sw.js
+++ public/sw.js
@@ -14,7 +14,6 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    // ensure we check the *right* cache to match against
     caches.open(cacheName).then(cache => {
       return cache.match(event.request).then(res => {
         return res || fetch(event.request)
